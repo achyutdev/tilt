5909de339d78a3c74e7057c370b02301e28d3159
Achyut Dev <mail_me@achyutdev.me>
2017-04-26 18:25:02 -0400

    inital commit

---
+++ src/main/java/dev/achyut/TiltApplication.java
@@ -0,0 +1,12 @@
+package dev.achyut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiltApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiltApplication.class, args);
+	}
+}
